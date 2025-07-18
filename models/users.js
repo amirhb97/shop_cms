c0b430fd46d18bdaad4554f330bc4210aae411cf
Amirhossein <amirhosseinhabibizadeh@gmail.com>
2025-07-18 14:46:43 +0330

    add-user-model

---
+++ models/users.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema({
+    fullName : {type:String , require:true},
+    email: {type:String , require:true, unique:true},
+    password : {type:String , require:true},
+    admin : {type : Boolean , default : false},
+},{timestamps:true});
+
+
+module.exports = model('User',userSchema);
